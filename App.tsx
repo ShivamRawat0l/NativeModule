0fddd19a2cf146a0c23e0368094030d8c3b1c450
Shivam Rawat <shivamrawat6670@gmail.com>
2026-05-11 23:55:47 +0530

    Handled the increment and decrement in the screen.

--- App.tsx
+++ App.tsx
@@ -1,45 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useRef, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const SKIP_COUNT = 5;
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+    const [count, setCount] = useState(0);
+    const incrementCount = useRef(0)
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+    const increment = () => {
+        incrementCount.current++;
+        if (incrementCount.current === SKIP_COUNT) {
+            setCount(prevCount => prevCount + SKIP_COUNT);
+            incrementCount.current = 0;
+        } else {
+            setCount(count + 1);
+        }
+    }
+
+    const decrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+
+    const reset = () => {
+        setCount(0);
+    }
+
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.container}>
+                    <Text style={styles.text}>{count}</Text>
+                    <Button title="Increment" onPress={increment} />
+                    <Button title="Decrement" onPress={decrement} />
+                    <Button title="Reset" onPress={reset} />
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
 });
 
 export default App;
